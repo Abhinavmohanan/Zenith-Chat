b0862e1eef3a2115fa991b5bdb289137b5809527
Abhinav <abhinavmohanan17@gmail.com>
2023-05-11 20:16:53 +0530

    home page UI complete , profile UI

---
+++ src/components/profile/index.tsx
@@ -0,0 +1,87 @@
+import logo from '../../assets/images/logo.png'
+import Image from 'next/image';
+import profile from '../../assets/images/profile.jpg'
+import TextareaAutosize from '@mui/base/TextareaAutosize';
+import { styled } from '@mui/system';
+import styles from './profile.module.css'
+import { FormControl, InputAdornment, OutlinedInput } from '@mui/material';
+import { AccountCircle, Close, Edit, EmojiEmotions, Search, Send } from '@mui/icons-material';
+import Input from '@mui/material/Input';
+import { Dispatch, SetStateAction, useRef } from 'react';
+
+type propsType = {
+  setProfileView: Dispatch<SetStateAction<boolean>>
+}
+
+const Profile = ({setProfileView }: propsType) => {
+    const blue = {
+        100: '#DAECFF',
+        200: '#b6daff',
+        400: '#3399FF',
+        500: '#007FFF',
+        600: '#0072E5',
+        900: '#003A75',
+      };
+    
+      const grey = {
+        50: '#f6f8fa',
+        100: '#eaeef2',
+        200: '#d0d7de',
+        300: '#afb8c1',
+        400: '#8c959f',
+        500: '#6e7781',
+        600: '#57606a',
+        700: '#424a53',
+        800: '#32383f',
+        900: '#24292f',
+      };
+    const searchRef = useRef<null| HTMLInputElement>(null);
+    const StyledTextarea = styled(TextareaAutosize)(
+        ({ theme }) => `
+        flex:1;
+        margin: 0 1rem;
+        resize: none;
+        box-sizing: border-box;
+        width:50%;
+        font-size:14px;
+        padding:5px 10px 5px 10px;
+        color:black;
+        height:10px;
+        background: rgba(255, 255, 255, 0.45);
+        border: 1px solid rgba(255, 255, 255, 0.46);
+        backdrop-filter: blur(7.4px);
+        /* Note: backdrop-filter has minimal browser support */
+        
+        border-radius: 10px;
+        &:hover {
+            border:none
+        }
+      
+        &:focus {
+          border:none;
+          box-shadow: 0 0 0 3px ${theme.palette.mode === 'dark' ? blue[500] : blue[200]};
+        }
+      
+        // firefox
+        &:focus-visible {
+          outline: 0;
+        }
+      `,
+      );
+    return (<>
+                <>
+                    <Close onClick={()=>{setProfileView(false)}}
+                     sx={{cursor:"pointer", color:"black",position:"absolute",top:"20px",right:"20px"}}/>
+                    <Image className={styles.profile} alt='' src={profile}/>
+                    <div className={styles.name}> Barry Allen, CSI</div>
+                    <div className={styles.phone}>
+                    <div className={styles.profile_item_heading}>Phone</div> <div className={styles.profile_item}> +91 922312312</div> <div><Edit sx={{marginRight:"10px",fontSize:"19px",color:"black"}}/></div>
+                    </div>
+                    <div className={styles.status}>
+                            <div className={styles.profile_item_heading}>Status</div> <div className={styles.profile_item}>  Hey I'm on Zenith , I'm the Fastest Man Alive , I'm the Flash <Edit sx={{marginRight:"10px",fontSize:"19px",color:"black"}}/></div> 
+                    </div>
+                </>
+    </>)
+}
+
+export default Profile;
--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -1,24 +1,97 @@
 import logo from '../../assets/images/logo.png'
 import Image from 'next/image';
 import profile from '../../assets/images/profile.jpg'
-
+import TextareaAutosize from '@mui/base/TextareaAutosize';
+import { styled } from '@mui/system';
 import styles from './homepage.module.css'
 import { FormControl, InputAdornment, OutlinedInput } from '@mui/material';
-import { AccountCircle, Search } from '@mui/icons-material';
+import { AccountCircle, EmojiEmotions, Search, Send } from '@mui/icons-material';
 import Input from '@mui/material/Input';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import { useRouter } from 'next/router';
+import { Transition } from '@headlessui/react'
+import Profile from '@/components/profile';
+function timeout(delay: number) {
+    return new Promise( res => setTimeout(res, delay) );
+}
 
 const HomePage = () => {
-    const searchRef = useRef<null| HTMLInputElement>(null);
+    const router = useRouter()
+    const [profileView,setProfileView] = useState(false);
 
-    return (<>
+    const blue = {
+        100: '#DAECFF',
+        200: '#b6daff',
+        400: '#3399FF',
+        500: '#007FFF',
+        600: '#0072E5',
+        900: '#003A75',
+      };
+    
+      const grey = {
+        50: '#f6f8fa',
+        100: '#eaeef2',
+        200: '#d0d7de',
+        300: '#afb8c1',
+        400: '#8c959f',
+        500: '#6e7781',
+        600: '#57606a',
+        700: '#424a53',
+        800: '#32383f',
+        900: '#24292f',
+      };
+    const searchRef = useRef<null| HTMLInputElement>(null);
+    const StyledTextarea = styled(TextareaAutosize)(
+        ({ theme }) => `
+        flex:1;
+        margin: 0 1rem;
+        resize: none;
+        box-sizing: border-box;
+        width:50%;
+        font-size:14px;
+        padding:5px 10px 5px 10px;
+        color:black;
+        height:10px;
+        background: rgba(255, 255, 255, 0.45);
+        border: 1px solid rgba(255, 255, 255, 0.46);
+        backdrop-filter: blur(7.4px);
+        /* Note: backdrop-filter has minimal browser support */
+        
+        border-radius: 10px;
+        &:hover {
+            border:none
+        }
+      
+        &:focus {
+          border:none;
+          box-shadow: 0 0 0 3px ${theme.palette.mode === 'dark' ? blue[500] : blue[200]};
+        }
+      
+        // firefox
+        &:focus-visible {
+          outline: 0;
+        }
+      `,
+      );
+    return (<> 
         <main className={`xl:h-screen flex flex-col  px-32 py-16 justify-center items-center max-lg:flex-col max-lg:px-0 max-sm:px-0`}>
             {/* <div className="logo"><Image src={logo} alt="" /></div> */}
-            <div className='flex items-center justify-around w-10/12 h-full bg-gray-100 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-2xl bg-opacity-30 border border-blue-200 max-lg:w-5/6 max-sm:w-11/12
+            <div className='overflow-hidden flex items-center justify-around w-11/12 h-full bg-gray-100 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-2xl bg-opacity-30 border border-blue-200 max-lg:w-5/6 max-sm:w-11/12
             '>
+                {profileView? <Transition  /*Need to fix transition*/
+                className={styles.section__1__profile}
+                enter="transition-opacity duration-75"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="transition-opacity duration-150"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0" show={profileView}>
+                    <Profile setProfileView={setProfileView}/>
+                </Transition>:
                 <div className={styles.section__1}>
                     <div className={styles.profile_bar}>
-                        <Image className={styles.profile} alt='' src={profile}/>
+                        <Image className={styles.profile} alt='' onClick={async()=>{
+                            setProfileView(!profileView)}} src={profile}/>
                         <div>
                         <FormControl sx={{border:"none",outline:"none"}}>
                             <Input
@@ -152,8 +225,41 @@ const HomePage = () => {
                         
                         </div>
                     </div>
-                </div>
+                </div>}
                 <div className={styles.section__2}>
+                    <div className={styles.chat_header}>
+                            <Image className={styles.profile_chat} alt='' src={profile}/>
+                            Name
+                    </div>
+                    <div className={styles.chat_body}>
+                        <div className={styles.chat_messages}>
+                                <div className={styles.chat_message}>Lorem ipsum dolor sit amet consectetur</div>
+                                <div className={styles.chat_message}>Lorem ipsum dolor  <br></br>sit amet consectetur</div>
+                                <div className={styles.chat_message}>Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur</div>
+                                <div className={styles.chat_message}>Lorem ipsum dolor sit amet consectetur</div>
+                                <div className={styles.chat_message}>Lorem ipsum dolor  <br></br>sit amet consectetur</div>
+                                <div className={styles.chat_message}>Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur</div>
+                                <div className={styles.chat_message}>Lorem ipsum dolor sit amet consectetur</div>
+                                <div className={styles.chat_message}>Lorem ipsum dolor  <br></br>sit amet consectetur</div>
+                                <div className={styles.chat_message}>Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur</div>
+                                <div className={styles.chat_message}>Lorem ipsum dolor sit amet consectetur</div>
+                                <div className={styles.chat_message}>Lorem ipsum dolor  <br></br>sit amet consectetur</div>
+                                <div className={styles.chat_message}>Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur</div>
+                                <div className={styles.chat_message}>Lorem ipsum dolor sit amet consectetur</div>
+                                <div className={styles.chat_message}>Lorem ipsum dolor  <br></br>sit amet consectetur</div>
+                                <div className={styles.chat_message}>Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur</div>
+                       
+                        </div>
+                        <div className={styles.chat_send}>
+                            <EmojiEmotions sx={{color:"rgba(44, 153, 221, 0.6)",cursor:"pointer"}}/>
+                            <StyledTextarea
+                            className={styles.styled_textarea}
+                            maxRows={3}
+                            placeholder="Type a message"
+                            />
+                            <Send sx={{color:"rgba(44, 153, 221, 0.5)",cursor:"pointer"}}/>
+                        </div>
+                    </div>
                 </div>
             </div>
         </main>
